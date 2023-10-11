bdc585c219799589da55da5937512d627c3695b8
The_Pr0fessor <46141975+TrendonRobinson@users.noreply.github.com>
2023-10-11 00:19:15 -0400

    Util

---
+++ src/Interface/Util/Sounds.lua
@@ -0,0 +1,10 @@
+--[[
+--Created Date: Wednesday October 11th 2023 12:19:00 am CEST
+--Author: Trendon Robinson at <The_Pr0fessor (Rbx), @TPr0fessor (Twitter)>
+-------
+--Last Modified: Wednesday October 11th 2023 12:19:11 am CEST
+--Modified By: Trendon Robinson at <The_Pr0fessor (Rbx), @TPr0fessor (Twitter)>
+--]]
+local Sounds = {}
+
+return Sounds
---
+++ src/Interface/Util/Tweens.lua
@@ -0,0 +1,10 @@
+--[[
+--Created Date: Wednesday October 11th 2023 12:13:28 am CEST
+--Author: Trendon Robinson at <The_Pr0fessor (Rbx), @TPr0fessor (Twitter)>
+-------
+--Last Modified: Wednesday October 11th 2023 12:18:50 am CEST
+--Modified By: Trendon Robinson at <The_Pr0fessor (Rbx), @TPr0fessor (Twitter)>
+--]]
+local Tweens = {}
+
+return Tweens
