06dc67d1fa1ca6949eadae7ded3765786ec33d75
The_Pr0fessor <46141975+TrendonRobinson@users.noreply.github.com>
2023-10-19 13:21:50 -0400

    Init

--- src/Interface/Stories/Root.lua
+++ src/Interface/Stories/Root.lua
@@ -2,11 +2,12 @@
 --Created Date: Wednesday October 11th 2023 12:14:55 am CEST
 --Author: Trendon Robinson at <The_Pr0fessor (Rbx), @TPr0fessor (Twitter)>
 -------
---Last Modified: Wednesday October 11th 2023 12:14:58 am CEST
+--Last Modified: Thursday October 19th 2023 1:13:06 pm CEST
 --Modified By: Trendon Robinson at <The_Pr0fessor (Rbx), @TPr0fessor (Twitter)>
 --]]
 --// Services
 local Plugin = script.Parent.Parent.Parent.Parent
+local Packages = Plugin.Packages
 
 --// Pages
 local Components = Plugin.Interface.Components
